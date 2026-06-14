7fc7f3af6357a11cc2294dbd64ece4edc7b09b75
klozz <carlosj@klozz.dev>
2026-06-14 15:51:04 -0600

    redwood: drop Firmware for now
    Signed-off-by: klozz <carlosj@klozz.dev>

--- Android.mk
+++ Android.mk
@@ -6,22 +6,6 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),redwood)
 
-$(call add-radio-file-sha1-checked,radio/abl.img,70e565cc54af923f0d2db5c679c7ffebdb26038d)
-$(call add-radio-file-sha1-checked,radio/aop.img,a51d583e679b35becc79fb535802c43456b0d903)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,7a720f05caca3d931ffd92728f1cb5dff8e9fde9)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,f3097d056e7cc1019ac4bfc847c9d239a84ca37b)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,aa80ef8f34edb813c349f8d8ced0fe65fe6ce5f8)
-$(call add-radio-file-sha1-checked,radio/dsp.img,d3fdbfa43b8686991d86c6679c3a87b07a11b368)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,004653d5f6a338abb4db6a83080369705a5a8c0b)
-$(call add-radio-file-sha1-checked,radio/hyp.img,e537d9f4830e377625e9ca4b981e55f1b390fa3b)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,244bf027a88a041e23e4bbe3cf25f44457d68d10)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,fdfaf0062409213cd36da42aef8d9e11956fd472)
-$(call add-radio-file-sha1-checked,radio/modem.img,a83b187df62689edfca51b4cb27b8e5680bff05d)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,e004ca7b8dc38c18ee57870c986e114199a88e6d)
-$(call add-radio-file-sha1-checked,radio/shrm.img,e2f63f66b5e06636941c1cb3bdade1df9ba77b99)
-$(call add-radio-file-sha1-checked,radio/tz.img,0a8c3df7109f008de38d80aaea16c7974f8d3c38)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,f4baf26af3c6e65b5b1f43452cb67991c7be9e26)
-$(call add-radio-file-sha1-checked,radio/xbl.img,607658aca9a20e42b224c9d392f72c19e5d760b7)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,d0266b0027d174451584629171677caeb0884fd8)
+
 
 endif
